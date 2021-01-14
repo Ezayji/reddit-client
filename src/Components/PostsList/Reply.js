985e5ffafc87df1c667d11c52afed8b2ff90aaf2
Alex Koppel <75754097+Ezayji@users.noreply.github.com>
2021-01-14 23:06:05 +0200

    Every post now has max 200 comments with all replies and subreplies

--- src/Components/PostsList/Reply.js
+++ src/Components/PostsList/Reply.js
@@ -7,21 +7,38 @@ const Reply = ({ reply }) => {
     const upVotes = numeral(reply.data.ups).format('0a');
     const date = new Date(reply.data.created_utc * 1000);
     
+    let subReply;
+
+    if (reply.data.replies){
+        subReply = reply.data.replies.data.children.map((reply, i) => (
+            <Reply reply={reply} key={i} />
+    ))
+    } else {
+        subReply = null; 
+    }
+
+    if(reply.kind.includes("more")){
+        return null;
+    }
+
     return(
-        <article className="comment reply" key={reply.data.id} >
-            <div className="comment-border"></div>
-            <div className="comment-content">
-                <div className="comment-creator">
-                    <p><span>{reply.data.author}</span> | <TimeAgo date={date} /></p>
+        <div className="reply">
+            <article className="comment answer" key={reply.data.id} >
+                <div className="comment-border"></div>
+                <div className="comment-content">
+                    <div className="comment-creator">
+                        <p><span>{reply.data.author}</span> | <TimeAgo date={date} /></p>
+                    </div>
+                    <p>{reply.data.body}</p>
+                    <div className="comment-ups">
+                        <div className="arrow"></div>
+                        <p className="ups upmargin"> {upVotes} </p>
+                        <div className="arrow2"></div>
+                    </div>
                 </div>
-                <p>{reply.data.body}</p>
-                <div className="comment-ups">
-                    <div className="arrow"></div>
-                    <p className="ups upmargin"> {upVotes} </p>
-                    <div className="arrow2"></div>
-                </div>
-            </div>
-        </article>
+            </article>
+            {subReply}
+        </div> 
     )
 }
 
